2de93476d2df76425de4a3f549ecc63917b68e8a
franko-bit <ngabofrank375@gmail.com>
2020-09-15 17:13:25 +0200

    Adding changes to index from queries
    -Adding Clearning codes

--- server/index.js
+++ server/index.js
@@ -2,20 +2,25 @@ const exp = require("express");
 const mongoosee = require("mongoose");
 const app = exp();
 const contro = require("./controller/controller.js");
+const controll = require("./controller/controllerArticle.js");
 mongoosee.connect(
-  "mongodb://localhost:27017/queries",
+  "mongodb://localhost:27017/My_Bland",
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
     if (err) {
-      console.log(err);
+      app.send(err);
     }
+    const PORT = 3000;
     app.use(exp.json());
-    app.get("/take", contro.GetQuery);
-    app.post("/upload", contro.createQuery);
-    app.get("/take/:_id", contro.findQuery);
+    app.get("/queries", contro.GetQuery);
+    app.post("/queries", contro.createQuery);
+    app.get("/queries/:_id", contro.findQuery);
+    app.get("/blog", controll.GetArticle);
+    app.post("/blog", controll.createArticle);
+    app.get("/blog/:_id", controll.findArticle);
 
-    app.listen(2000, () => {
-      console.log("Now connected");
+    app.listen(PORT, () => {
+      console.log("Server running on port ", PORT);
     });
   }
 );
